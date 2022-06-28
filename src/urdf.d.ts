dea5c6102cea2dd89a7c69c6b05b6711141dc717
Isabel Paredes <iparedes314@gmail.com>
2022-06-28 11:34:07 +0200

    Update example to use amphion

--- src/urdf.d.ts
+++ src/urdf.d.ts
@@ -2,3 +2,11 @@ declare module '*.urdf' {
     const value: string;
     export default value;
 }
+
+declare module '@robostack/amphion';
+
+declare module '@robostack/amphion/src/core/scene';
+
+declare module '@robostack/amphion/src/viz/RobotModel';
+
+declare module '@robostack/amphion/src/viewers/3d';
--- src/widget.ts
+++ src/widget.ts
@@ -12,25 +12,32 @@ import { Signal } from '@lumino/signaling';
 
 // import robot from 'src/robot.urdf';
 
-import {
-    WebGLRenderer,
-    PerspectiveCamera,
-    Scene,
-    Mesh,
-    PlaneBufferGeometry,
-    ShadowMaterial,
-    DirectionalLight,
-    PCFSoftShadowMap,
-    sRGBEncoding,
-    Color,
-    AmbientLight,
-    Box3,
-    LoadingManager,
-    MathUtils,
-    // Loader,
-} from 'three';
+// import {
+//     WebGLRenderer,
+//     PerspectiveCamera,
+//     // Scene,
+//     Mesh,
+//     PlaneBufferGeometry,
+//     ShadowMaterial,
+//     DirectionalLight,
+//     PCFSoftShadowMap,
+//     sRGBEncoding,
+//     Color,
+//     AmbientLight,
+//     Box3,
+//     LoadingManager,
+//     MathUtils,
+//     // Loader,
+// } from 'three';
 
-import URDFLoader from "urdf-loader"
+// import URDFLoader from "urdf-loader"
+
+// import Amphion from '@robostack/amphion';
+import Viewer3d from '@robostack/amphion/src/viewers/3d';
+import Scene from '@robostack/amphion/src/core/scene';
+import ROSLIB from 'roslib';
+import RobotModel from '@robostack/amphion/src/viz/RobotModel';
+// import CONFIG from '@robostack/amphion/examples/config.json';
 
 
 
@@ -77,80 +84,80 @@ export class UrdfPanel extends Widget {
     
 
         // TODO
-        // const obj = this._context.model.getSharedObject();
         this._canvas = document.createElement('div');
         this._canvas.className = 'jp-urdf-canvas';
         this._canvas.style.left = '0px';
         this._canvas.innerText = 'URDF ROBOT';
         // this.node.appendChild(this._canvas);
 
-        // URDF Loader
+
+        // Amphion
+        this._ros = new ROSLIB.Ros({});
         this._scene = new Scene();
-        this._scene.background = new Color(0x263238);
-
-        this._camera = new PerspectiveCamera();
-        this._camera.position.set(10, 10, 10);
-        this._camera.lookAt(0, 0, 0);
-
-        this._renderer = new WebGLRenderer({ antialias: true });
-        this._renderer.outputEncoding = sRGBEncoding;
-        this._renderer.shadowMap.enable = true;
-        this._renderer.shadowMap.type = PCFSoftShadowMap;
-
-        const directionalLight = new DirectionalLight(0xffffff, 1.0);
-        directionalLight.castShadow = true;
-        directionalLight.shadow.mapSize.setScalar(1024);
-        directionalLight.position.set(5, 30, 5);
-        this._scene.add(directionalLight);
-
-        const ambientLight = new AmbientLight(0xffffff, 0.2);
-        this._scene.add(ambientLight);
-
-        const ground = new Mesh(new PlaneBufferGeometry(), 
-                                new ShadowMaterial({ opacity: 0.25}));
-        ground.rotation.x = -Math.PI / 2;
-        ground.scale.setScalar(30);
-        ground.receiveShadow = true;
-        this._scene.add(ground);
-
-        // TODO import orbit controls
-
-        // Load Robot
-        this._manager = new LoadingManager();
-        this._loader = new URDFLoader(this._manager);
-         
-        // this._loader.load(context.path, (result: any) => {
-        //     this._robot = result;
-        // });
-        
-        this._robot = this._loader.parse(context.model.value.text);
-
-
-        // Wait until all geometry has been loaded to add model to scene
-        this._manager.onLoad = () => {
-            this._robot.rotation.x = Math.PI / 2;
-            this._robot.traverse((c : any) => {
-                c.castShadow = true;
-            });
-            for (let i = 1; i <=6; i++) {
-                this._robot.joints[`HP${ i }`].setJointValue(MathUtils.degToRad(30));
-                this._robot.joints[`KP${ i }`].setJointValue(MathUtils.degToRad(120));
-                this._robot.joints[`AP${ i }`].setJointValue(MathUtils.degToRad(-60));
+        this._viewer = new Viewer3d(this._scene);
+        this._robot = new RobotModel(this._ros, 'robot_description',
+            {
+                packages: {franka_description: 'http://localhost:8888/api/ros/pkgs/franka_description'},
             }
-            this._robot.updateMatrixWorld(true);
+        );
 
-            const bb = new Box3();
-            bb.setFromObject(this._robot);
+        this._robot.loadFromParam();
+        this._viewer.addVisualization(this._robot);
+        // this._viewer.initRenderer();
+        // this.node.appendChild(this._viewer.renderer.domElement);
+        this._viewer.setContainer(this.node);
 
-            this._robot.position.y -= bb.min.y;
-            this._scene.add(this._robot);
-        };
 
-        // TODO add onResize() 
+        // // URDF Loader
+        // this._scene = new Scene();
+        // this._scene.background = new Color(0x263238);
+
+        // this._camera = new PerspectiveCamera();
+        // this._camera.position.set(10, 10, 10);
+        // this._camera.lookAt(0, 0, 0);
+
+        // this._renderer = new WebGLRenderer({ antialias: true });
+        // this._renderer.outputEncoding = sRGBEncoding;
+        // this._renderer.shadowMap.enable = true;
+        // this._renderer.shadowMap.type = PCFSoftShadowMap;
+
+        // const directionalLight = new DirectionalLight(0xffffff, 1.0);
+        // directionalLight.castShadow = true;
+        // directionalLight.shadow.mapSize.setScalar(1024);
+
+        // directionalLight.position.set(5, 30, 5);
+        // this._scene.add(directionalLight);
+
+        // const ambientLight = new AmbientLight(0xffffff, 0.2);
+        // this._scene.add(ambientLight);
+
+        // const ground = new Mesh(new PlaneBufferGeometry(), 
+        //                         new ShadowMaterial({ opacity: 0.25}));
+        // ground.rotation.x = -Math.PI / 2;
+        // ground.scale.setScalar(30);
+        // ground.receiveShadow = true;
+        // this._scene.add(ground);
+
+        // // TODO import orbit controls
+
+        // // Load Robot
+        // this._manager = new LoadingManager();
+        // this._loader = new URDFLoader(this._manager);
+         
+        // // this._loader.load(context.path, (result: any) => {
+        // //     this._robot = result;
+        // // });
         
-        this._renderer.render(this._scene, this._camera);
+        // this._robot = this._loader.parse(context.model.value.text);
 
-        this.node.appendChild(this._renderer.domElement);
+
+        // // Wait until all geometry has been loaded to add model to 
+
+        // // TODO add onResize() 
+        
+        // this._renderer.render(this._scene, this._camera);
+
+        // this.node.appendChild(this._renderer.domElement);
         
 
     }
@@ -206,11 +213,18 @@ export class UrdfPanel extends Widget {
     // private _cube: HTMLElement;
 
     // URDF Loader variables
-    private _scene: any;
-    private _camera: any;
-    private _renderer: any;
-    private _robot: any;
+    // private _scene: any;
+    // private _camera: any;
+    // private _renderer: any;
+    // private _robot: any;
     // private _controls: any;
-    private _manager: any;
-    private _loader: any;
+    // private _manager: any;
+    // private _loader: any;
+
+    // Amphion
+    private _scene: Scene;
+    private _viewer: Viewer3d;
+    private _robot: RobotModel;
+    private _ros: any;
+
 }
